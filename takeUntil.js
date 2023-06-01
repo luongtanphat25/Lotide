c300973789e1daacc166d0de8a8242641fdb4cfd
Phat Luong <luongtanphat25@gmail.com>
2023-06-01 16:00:56 -0400

    takeUntil completed

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const eqArrays = function(firstArr, secondArr) {
+  let result = (firstArr.length === secondArr.length);
+  for (let i = 0; i < firstArr.length; i++) {
+    result = result && (firstArr[i] === secondArr[i]);
+  }
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
