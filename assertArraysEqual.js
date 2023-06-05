09415ea80b5af6c8c75e9b0d6d17ccaf6c6390b0
Phat Luong <luongtanphat25@gmail.com>
2023-06-05 12:02:52 -0400

    modified assertArraysEqual and created test file for it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-const eqArrays = function(firstArr, secondArr) {
-  let result = (firstArr.length === secondArr.length);
-  for (let i = 0; i < firstArr.length; i++) {
-    result = result && (firstArr[i] === secondArr[i]);
-  }
-  return result;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -13,3 +7,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
