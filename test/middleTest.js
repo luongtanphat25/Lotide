ea42c26896fd3df44f6dd44764835eadc8df6eac
Phat Luong <luongtanphat25@gmail.com>
2023-06-05 16:14:17 -0400

    refactor middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
+const eqArrays = require('../eqArrays');
 const middle = require('../middle.js');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);//1.5 -> 1
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);//2
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('returns [] for [1]', ()=> {
+    assert.isTrue(eqArrays(middle([1]), []));
+  });
+  it('returns [] for [1, 2]', ()=> {
+    assert.isTrue(eqArrays(middle([1, 2]), []));
+  });
+  it('returns [2] for [1, 2, 3]', ()=> {
+    assert.isTrue(eqArrays(middle([1, 2, 3]), [2]));
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', ()=> {
+    assert.isTrue(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
+  });
+});
