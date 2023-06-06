02d14a36cd189c23ec4ab90d7d23b3331b5a9e53
Phat Luong <luongtanphat25@gmail.com>
2023-06-05 20:33:25 -0400

    refactor eqArrayTest

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,7 +1,26 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe('#eqObjects Test', () => {
+  it(`[1, 2, 3] equals [1, 2, 3]`, () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it(`[1, 2, 3] not equals [3, 2, 1]`, () => {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+
+  it(`["1", "2", "3"] equals ["1", "2", "3"]`, () => {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+
+  it(`["1", "2", "3"] not equals ["1", "2", "3"]`, () => {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 1]));
+  });
+});
+
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
